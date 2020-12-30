b09a4de4af3f830e7bf7ca88c9eec16a98ed7327
Luiz Nascimento <luizh.nnh@gmail.com>
2020-12-30 12:12:48 -0300

    feat: cadastro de localizacoes(localidades)

--- src/routes/localizations.routes.ts
+++ src/routes/localizations.routes.ts
@@ -1,5 +1,23 @@
 import { Router } from 'express';
+import authenticated from 'middlewares/authenticated';
+import CreateLocalizationService from 'services/CreateLocalizationService';
 
 const localizationsRouter = Router();
+
+localizationsRouter.use(authenticated);
+
+localizationsRouter.post('/', async (request, response) => {
+  try {
+    const { name, long, lat } = request.body;
+
+    const createLocalization = new CreateLocalizationService();
+
+    const localization = await createLocalization.execute({ name, long, lat });
+
+    return response.send(localization);
+  } catch (err) {
+    return response.status(404).json({ error: err.message });
+  }
+});
 
 export default localizationsRouter;
---
+++ src/services/CreateLocalizationService.ts
@@ -0,0 +1,22 @@
+import Localization from 'models/Localization';
+import { getRepository } from 'typeorm';
+
+interface Request {
+  name: string;
+  long: string;
+  lat: string;
+}
+
+class CreateLocalizationService {
+  public async execute({ name, long, lat }: Request): Promise<Localization> {
+    const localizationRepository = getRepository(Localization);
+
+    const localization = localizationRepository.create({ name, long, lat });
+
+    await localizationRepository.save(localization);
+
+    return localization;
+  }
+}
+
+export default CreateLocalizationService;
