a57e89f8c95579e5e75f12f24767bcf8dbe60b8e
Luiz Nascimento <luizh.nnh@gmail.com>
2021-01-02 15:20:04 -0300

    chore: mudou app.listen para poder definir a porta do heroku

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -24,6 +24,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   return response.status(500).json({ error: 'error', message: err });
 });
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('Server Started');
 });
